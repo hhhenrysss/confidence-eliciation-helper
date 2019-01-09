fbbd9f6eb5be4f058cb21b6587988183b9a0517d
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2019-01-08 18:42:27 -0600

    added submit

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,8 @@
 require('./style.scss');
 import * as $ from "jquery";
 import {DirectoryOperations} from "./utils";
+import {Processing} from "./computing";
+
 
 const handle_open_directory = (dir_obj: DirectoryOperations) => {
     const $open_directory = $('#open_directory');
@@ -12,7 +14,7 @@ const handle_open_directory = (dir_obj: DirectoryOperations) => {
 };
 
 const handle_save_directory = (dir_obj: DirectoryOperations) => {
-    const $save_directory = $('#save_directory');
+    const $save_directory = $('#save_directory button');
     $save_directory.on('click', (event) => {
         const path = dir_obj.getSaveDirectory();
         const $this = $(event.currentTarget);
@@ -21,11 +23,22 @@ const handle_save_directory = (dir_obj: DirectoryOperations) => {
 };
 
 const handle_open_answer_file = (dir_obj: DirectoryOperations) => {
-    const $open_file = $('#open_answer_file');
+    const $open_file = $('#open_answer_file button');
     $open_file.on('click', (event) => {
         const path = dir_obj.getAnswerFilePath();
         const $this = $(event.currentTarget);
         $this.siblings('.message').html(`<strong>Answer file path: ${path}</strong>`);
+    });
+};
+
+const handle_submit = (dir_obj: DirectoryOperations) => {
+    const $btn = $('#submit_task button');
+    $btn.on('click', (event) => {
+        const processing = new Processing(dir_obj, 'json');
+        $('#submit_task .message').html('Processing files ...');
+        processing.processingDir().then((dest_file_name) => {
+            $('#submit_task .message').html(`Successfully generated ${dest_file_name} at ${dir_obj.getSaveDirectory()}`);
+        });
     });
 };
 
@@ -35,5 +48,6 @@ window.addEventListener('load', function () {
     handle_open_directory(dir_obj);
     handle_save_directory(dir_obj);
     handle_open_answer_file(dir_obj);
+    handle_submit(dir_obj);
 });
 
